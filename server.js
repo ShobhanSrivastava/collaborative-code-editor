721ed97d620a09bb9883518552212047e4cf102c
Shobhan <srivastavashobhan2001@gmail.com>
2022-09-17 16:10:33 +0530

    Preparing to deploy

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const { Server } = require('socket.io');
 const http = require('http');
 const ACTIONS = require('./src/Actions');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(express.static('build'));
+app.use((req, res, next)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'));
+})
 
 const io = new Server(server);
 
